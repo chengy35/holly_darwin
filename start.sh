d34f9ab5ddeee2b8fd6db104a406dbf267ed130d
chengy35 <chengy35@mail2.sysu.edu.cn>
2016-08-28 11:21:49 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd improved_trajectory
-bash run.sh
-cd ..
+#cd improved_trajectory
+#bash run.sh
+#cd ..
 cd GMM
 bash run.sh
 cd ..
@@ -9,6 +9,9 @@ cd FV
 bash run.sh
 cd ..
 cd Darwin
+bash run.sh
+cd ..
+cd process
 bash run.sh
 cd ..
 cd trainAndTest
